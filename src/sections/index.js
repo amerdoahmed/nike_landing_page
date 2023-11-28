71eff062d404cdaee8b148a82532bf9fb44bccf0
ahmed <ahmedamerdo0@gmail.com>
2023-11-28 22:00:07 +0100

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export{
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    SpecialOffers,
+    SuperQuality,
+    Services,
+    Subscribe,
+    Footer
+}
+
